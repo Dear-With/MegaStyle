cdb4583a8cfbf36b99459e05fcdbf6e1febca479
Dear-With <sousukekousyunta190213@docomo.ne.jp>
2025-02-11 18:29:45 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$('video').hide();
+$('video').slideDown();
+$('#A1').hide();
+$('#Q1').click(function() {
+      $('#A1').slideDown();
+})
+$('#A2').hide();
+$('#Q2').click(function() {
+      $('#A2').slideDown();
+})
+$('#A3').hide();
+$('#Q3').click(function() {
+      $('#A3').slideDown();
+})
